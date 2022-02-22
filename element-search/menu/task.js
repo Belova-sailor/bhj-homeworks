436c968b9389f07e4cd783da5027a315dc8d7c63
Belova <katishen@mail.ru>
2022-02-22 18:39:01 +0500

    menu changes

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,17 +4,19 @@ for (let index = 0; index < menuLink.length; index++) {
     const getName = function (index) {
         let name = menuLink[index];
         return name;
-    }
+    };
 
     let link = getName(index);
 
     link.onclick = () => {
         let sub = link.parentElement.querySelector('.menu_sub');
-        if (sub !== null) {
+        menuActive = link.parentElement.querySelector('.menu_active');
+
+        if (!menuActive && sub !== null) {
             let subCollection = document.querySelectorAll('.menu_sub');
             subCollection.forEach(item => item.classList.remove('menu_active'));
             sub.classList.add('menu_active');
             return false;
         };
     };
-}
+} 
