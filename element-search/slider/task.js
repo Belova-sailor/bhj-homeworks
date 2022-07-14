ead79f09f80efa73c6b361d8f25b0b84e33a4b04
Belova <katishen@mail.ru>
2022-07-14 18:51:17 +0500

    changes

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,30 +1,33 @@
+const sliderItem = Array.from(document.querySelectorAll('.slider__item'));
 const sliderLeft = document.querySelector('.slider__arrow_prev');
 const sliderRight = document.querySelector('.slider__arrow_next');
-const sliderItems = document.getElementsByClassName('slider__item');
-let activeImg = 0;
+
+let activeSlider = sliderItem.findIndex((index) => {
+    return index.className === "slider__item slider__item_active"
+});
 
 const changeImgRight = function () {
-    sliderItems[activeImg].classList.remove('slider__item_active');
-    activeImg += 1;
+    sliderItem[activeSlider].classList.remove('slider__item_active');
+    activeSlider += 1;
 
-    if (activeImg === sliderItems.length) {
-        activeImg = 0;
+    if (activeSlider === sliderItem.length) {
+        activeSlider = 0;
     };
 
-    sliderItems[activeImg].classList.add('slider__item_active');
-    return activeImg;
+    sliderItem[activeSlider].classList.add('slider__item_active');
+    return activeSlider;
 };
 
 const changeImgLeft = function () {
-    sliderItems[activeImg].classList.remove('slider__item_active');
-    activeImg -= 1;
+    sliderItem[activeSlider].classList.remove('slider__item_active');
+    activeSlider -= 1;
 
-    if (activeImg < 0) {
-        activeImg = sliderItems.length - 1;
+    if (activeSlider < 0) {
+        activeSlider = sliderItem.length - 1;
     };
 
-    sliderItems[activeImg].classList.add('slider__item_active');
-    return activeImg;
+    sliderItem[activeSlider].classList.add('slider__item_active');
+    return activeSlider;
 };
 
 sliderLeft.addEventListener('click', changeImgLeft);
