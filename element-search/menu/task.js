00357da34e40569c7d27e0185e2d36248476f12a
Belova <katishen@mail.ru>
2022-07-14 17:48:22 +0500

    changes

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,22 +1,14 @@
 const menuLink = Array.from(document.querySelectorAll('.menu__link'));
 
-for (let index = 0; index < menuLink.length; index++) {
-    const getName = function (index) {
-        let name = menuLink[index];
-        return name;
-    };
-
-    let link = getName(index);
-
+menuLink.forEach((link) => {
     link.onclick = () => {
         let sub = link.parentElement.querySelector('.menu_sub');
-        menuActive = link.parentElement.querySelector('.menu_active');
 
-        if (!menuActive && sub !== null) {
+        if (sub) {
             let subCollection = document.querySelectorAll('.menu_sub');
             subCollection.forEach(item => item.classList.remove('menu_active'));
             sub.classList.add('menu_active');
             return false;
         };
     };
-} 
+})
