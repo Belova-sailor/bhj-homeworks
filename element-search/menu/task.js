8d2e861e8900e6b05bb01adf98bca096fa9a562b
Belova <katishen@mail.ru>
2022-07-15 16:02:18 +0500

    one more fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,12 +3,18 @@ const menuLink = Array.from(document.querySelectorAll('.menu__link'));
 menuLink.forEach((link) => {
     link.onclick = () => {
         let sub = link.parentElement.querySelector('.menu_sub');
+        let menuActive = link.parentElement.querySelector('.menu_active');
 
         if (sub) {
-            let subCollection = document.querySelectorAll('.menu_sub');
-            subCollection.forEach(item => item.classList.remove('menu_active'));
-            sub.classList.add('menu_active');
+            if (menuActive) {
+                sub.classList.remove('menu_active');
+            } else {
+                let subCollection = document.querySelectorAll('.menu_sub');
+                subCollection.forEach(item => item.classList.remove('menu_active'));
+                sub.classList.add('menu_active');
+            };
+
             return false;
-        };
+        }
     };
 })
